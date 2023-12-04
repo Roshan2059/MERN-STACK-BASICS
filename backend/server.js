901e3b67439f95730e6f0faae87b618ba96cd138
Roshan Panta <roshanpanta2059@gmail.com>
2023-12-04 15:29:32 +0545

    added comments

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
+//hamlai jun jun package chainxa teslai require() garney
 const express = require("express");
 
+//tala ko express() vaneko chai mathiko const express vanera lekhya xa ni!! ho tyo ho
 const app = express();
 
+// yesle chai requests haru lai listen gardo raixa
 app.listen(4000, () => {
   console.log("Roshan, timi dhukka vara code gara ma listen gardai xu!!");
+});
+
+//yespaxi chai euta nodemon vanni package install gariyoo
+
+//tespaxi chai simply django ma  garya jastai url routes haryu dinu pardo raixa!!
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the first app that roshan made in MERN" });
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>This is about page</h1>");
 });
